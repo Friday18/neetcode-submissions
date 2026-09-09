e7e00e063fcd2ef863366352d1791b1c4de8ab7a
Friday18 <34274245+Friday18@users.noreply.github.com>
2026-09-09 23:29:55 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+        if s == t:
+            return True
+
+        mapOfS = {}
+
+        for i in s:
+            mapOfS[i] = mapOfS.get(i,0)+1
+        
+        mapOfT = {}
+        for i in t:
+            mapOfT[i] = mapOfT.get(i, 0)+1
+        
+        return mapOfS == mapOfT
